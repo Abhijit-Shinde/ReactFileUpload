9e33b797b0e4e2a20173019c94e5bfd3b9e511e5
GirdharBCT <Girdhar.Sahu@BlueconchTech.com>
2022-03-04 14:27:57 +0530

    restructure

--- src/api/index.js
+++ src/api/index.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    
+    baseURL: 'https://localhost:5001/s3/',
     headers: {
       Accept: 'application/json'
     }
   });
 
 const createBucketApi = (bucketName) =>{
-    return api.post("https://localhost:5001/s3/Create?bucketName="+bucketName)
+    return api.post("Create?bucketName="+bucketName)
 }
 
 const uploadFile = (bucketName,formData,handleUploadProgress = ()=>{}) =>{
-    return api.post("https://localhost:5001/s3/AddFile?bucketName="+bucketName, formData, {
+    return api.post("AddFile?bucketName="+bucketName, formData, {
             onUploadProgress: handleUploadProgress
           })
 }
